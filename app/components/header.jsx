2211654c5b3a3eb36c0a4438c9da753bd3cb0fd7
finleyc24 <104926702+finleyc24@users.noreply.github.com>
2025-06-22 22:29:52 -0400

    Create nav bar with links to othe pages

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -5,6 +5,19 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <h1>Cole Finley</h1>
+      <nav>
+        <ul className={styles.navList}>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
